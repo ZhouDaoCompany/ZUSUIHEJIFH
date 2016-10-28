59dfc86dc7b2e79f99f023a7638eb6cd83de1136
ZhouDaoHuiFa <1024463782@qq.com>
2016-10-28 09:13:09 +0800

    wenjian

--- SwiftControls/Pods/Target Support Files/Pods-SwiftControlsTests/Pods-SwiftControlsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftControlsTests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftControlsTests
-@end
--- SwiftControls/Pods/Target Support Files/Pods-SwiftControlsUITests/Pods-SwiftControlsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftControlsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftControlsUITests
-@end
